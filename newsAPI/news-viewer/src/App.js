be7473f9d16ace37a8b4474cf1908505785194f6
dndkwk <rmswn3147@gmail.com>
2021-01-22 22:39:27 +0900

    뉴스페이지만들어보기

---
+++ newsAPI/news-viewer/src/App.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import NewsList from './components/NewsList';
+
+const App = () => {
+  //9fe3f07998f74b429d50e9a34d549e61
+  return <NewsList />;
+};
+
+export default App;
