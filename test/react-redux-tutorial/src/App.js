b8abf572bd3c8f0eecd19248244b728942127035
dndkwk <rmswn3147@gmail.com>
2021-02-10 18:27:25 +0900

    no message

--- test/react-redux-tutorial/src/App.js
+++ test/react-redux-tutorial/src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import Counter from './components/Counter'
-import Todos from './components/Todos'
-
+import Todos from './components/Todos';
+import CounterContainer from './containers/CounterContainer';
 const App = () => {
   return (
-  <div>
-      <Counter number={0}/>
-      <hr/>
-      <Todos/>
+    <div>
+      <CounterContainer />
+      <hr />
+      <Todos />
     </div>
   );
 };
 
 export default App;
-
